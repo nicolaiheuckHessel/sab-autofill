e914a1554239ac778cc7e8295cb1c17a33fdf37a
nicolaiheuck <nicolaiheuck@gmail.com>
2022-06-23 17:58:17 +0200

    Added .crx file

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,34 +1,3 @@
-
-// // Extension port to communicate with the popup, also helps detecting when it closes
-// let port = null;
-
-// // Send messages to the open port (Popup)
-// const sendPortMessage = data => port.postMessage(data);
-
-// // Handle incoming popup messages
-// const popupMessageHandler = message => console.log('in-content.js - message from popup:', message);
-
-// // Start scripts after setting up the connection to popup
-// chrome.extension.onConnect.addListener(popupPort => {
-//     // Listen for popup messages
-//     popupPort.onMessage.addListener(popupMessageHandler);
-//     // Set listener for disconnection (aka. popup closed)
-//     popupPort.onDisconnect.addListener(() => {
-//         console.log('in-content.js - disconnected from popup');
-//     });
-//     // Make popup port accessible to other methods
-//     port = popupPort;
-//     // Perform any logic or set listeners
-//     sendPortMessage('message from in-content.js');
-// });
-
-// // Response handler for short lived messages
-// const handleBackgroundResponse = response =>
-//     console.log('in-content.js - Received response:', response);
-
-// // Send a message to background.js
-// chrome.runtime.sendMessage('Message from in-content.js!', handleBackgroundResponse);
-
 const isSAB = () => {
     const testField = document.getElementById("YourCar:Email");
     return testField !== null;
