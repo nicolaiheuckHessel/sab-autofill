5a38143dcf9f9e700667e45407acfa0c9f2a2f61
Nicolai Heuck <n.heuck@hessel.dk>
2022-06-29 15:09:03 +0200

    Work in progress

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -20,16 +20,69 @@ const createButton = () => {
     const button = document.createElement("button")
     button.innerText = "Fill Form";
     button.style = "width: max-content;margin-left:auto;margin-left: auto; text-transform: uppercase; text-decoration: none; padding: 10px 30px; height: 45px; border-radius: 10rem; transition: all 0.2s ease 0s; font-size: 13px; font-weight: 700; letter-spacing: 0.3px; line-height: 18px; text-align: center; color: rgb(250, 250, 250); background-color: rgb(0, 137, 209); border: none; width: auto; min-width: 150px;cursor:pointer;";
-    button.onclick = () => fillForm();
-    document.getElementsByTagName("header")[0].children[0].append(button);
+    button.onclick = () => fillForm(1);
+    return button;
+}
+const createSelect = () => {
+    const select = document.createElement("select");
+    select.style = "width: 200px";
+    console.log("Creating empty option");
+    select.options.add(document.createElement("option"))
+    console.log("Looping profiles");
+    for (const profile of profiles) {
+        console.log("Loop profile " + profile.name);
+    select.options.add(createOption(profile.name, profile.id))
+    }
+    console.log("Profiles looped");
+
+    select.onchange = (e) => fillForm(e.target.value);
+    return select;
+}
+const injectElements = () => {
+    console.log("Creating div");
+    const container = document.createElement("div");
+    console.log("Creating select");
+    container.append(createSelect());
+    console.log("Created select");
+    // container.append(createButton());
+    console.log("Injecting div");
+    document.getElementsByTagName("header")[0].children[0].append(container);
+}
+const createOption = (name, id) => {
+    const option = document.createElement("option");
+    option.innerText = name;
+    option.value = id;
+    return option;
 }
 
-const fillForm = () => {
+const profiles = [
+    {
+        id: 1,
+        name: "Default profile",
+        email: "c.holmgaard@hessel.dk",
+        phone: '40283135',
+        regNr: 'ca88874',
+        kmDriven: '1234',
+        workshop: 'Randers'
+    },
+    {
+        id: 2,
+        name: "Correct profile",
+        email: "c.holmgaard@hessel.dk",
+        phone: '28403135',
+        regNr: 'ca88874',
+        kmDriven: '1234',
+        workshop: 'Randers'
+    },
+];
+
+const fillForm = (profileIndex) => {
+    const profile = profiles[profileIndex];
     const fieldAndValues = [
-        ["YourCar:Email", "c.holmgaard@hessel.dk"],
-        ["YourCar:Telephone number", "40283135"],
-        ["YourCar:Car Registration Number", "ca88874"],
-        ["YourCar:Driven Kilometers", "1234"]
+        ["YourCar:Email", profile.email],
+        ["YourCar:Telephone number", profile.phone],
+        ["YourCar:Car Registration Number", profile.regNr],
+        ["YourCar:Driven Kilometers", profile.kmDriven]
     ];
     const multiplier = 50;
     let i = 0;
@@ -48,9 +101,10 @@ const fillForm = () => {
         dispatchFocusEvent();
     }, multiplier * i);
 }
+
 const main = () => {
     if (!isSAB) return;
 
-    createButton();
+    injectElements();
 }
 main();
